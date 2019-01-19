329f7279c4d8037e1080fcd54480c4bcac798a60
j39m <hikaph@gmail.com>
2019-01-19 15:13:30 -0800

    naklo: Subclass libnaklo.Song to create mutagen-based taggers.

--- libnaklo/song.py
+++ libnaklo/song.py
@@ -4,6 +4,10 @@ This file implements the abstraction of a single song to be tagged.
 
 import sys
 import subprocess
+import os.path
+import mutagen
+import mutagen.flac
+import mutagen.easyid3
 
 __all__ = [
     "songs_to_array",
@@ -32,19 +36,11 @@ VALID_TAGS = set((
 ))
 
 
-def songs_to_array(*fnames, use_metaflac=True):
-    """
-    Given one or more paths to songs, return an array consisting of each
-    path initialized as a song object.
-    """
-    if use_metaflac:
-        return [MetaflacFlacSong(fname) for fname in fnames]
-    raise NotImplementedError("TODO(j39m): use mutagen.")
-
-
 class Song():
     """
-    A Song object applies to one file and can hold its tags.
+    A Song object applies to one file and can hold its tags. You
+    initialize Song objects (and its subclasses) with the path to
+    the file being tagged.
 
     Do not use this object by itself. Subclass it appropriately.
     """
@@ -109,3 +105,54 @@ class MetaflacFlacSong(Song):
         if meta.wait():
             efmt = "tag failed on ``{}:'' {}"
             raise IOError(efmt.format(self.path, stderr))
+
+class MutagenFlacSong(Song):
+    """
+    A MutagenFlacSong is a Song that identifies as FLAC and which uses
+    mutagen to tag itself.
+    """
+    def __init__(self, path):
+        super().__init__(path)
+        self.mutagen_obj = mutagen.flac.FLAC(path)
+
+    def do_tag(self, dry_run=False):
+        raise NotImplementedError("MutagenFlacSong.do_tag()")
+
+class MutagenMp3Song(Song):
+    """
+    A MutagenMp3Song is a Song that identifies as mp3 and which uses
+    mutagen to tag itself.
+    """
+    def __init__(self, path):
+        super().__init__(path)
+        self.mutagen_obj = mutagen.easyid3.EasyID3(path)
+
+    def do_tag(self, dry_run=False):
+        raise NotImplementedError("MutagenMp3Song.do_tag()")
+
+EXT_MUTAGEN_MAP = {
+    ".flac": MutagenFlacSong,
+    ".mp3": MutagenMp3Song,
+}
+
+def song_to_mutagen_file(fname):
+    """Given a filename, return a Mutagen file object of that file."""
+    (_, ext) = os.path.splitext(fname)
+    try:
+        fclass = EXT_MUTAGEN_MAP[ext]
+    except KeyError:
+        raise IOError("Bad extension: ``{}''".format(ext))
+    return fclass(fname)
+
+def songs_to_array(*fnames, use_metaflac=True):
+    """
+    Given one or more paths to songs, return an array consisting of each
+    path initialized as a song object.
+    """
+    if use_metaflac:
+        return [MetaflacFlacSong(fname) for fname in fnames]
+
+    retv = list()
+    for fname in fnames:
+        retv.append(song_to_mutagen_file(fname))
+    return retv
